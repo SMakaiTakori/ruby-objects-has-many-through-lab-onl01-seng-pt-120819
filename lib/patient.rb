44b123c43a164871b5326af37524771eac55c17f
S. Makai Takori <makait87@gmail.com>
2020-02-01 05:24:42 +0000

    Automatically backed up by Learn

---
+++ lib/patient.rb
@@ -0,0 +1,32 @@
+class Patient
+  
+  @@all = []  
+
+  attr_accessor :appointment, :doctor
+  attr_reader :name
+  
+  def initialize(name)
+    @name = name
+    @@all << self
+  end  
+  
+  def new_appointment(doctor, date)
+    Appointment.new(self, doctor, date)
+  end
+  
+  def appointments
+    Appointments.all.select do |appointment|
+      appointment.patient == self
+    end
+  end  
+  
+  def doctors
+    appointments.map do |appointment|
+      appointment.doctor
+    end
+  end
+
+  def self.all
+    @@all
+  end
+end  
